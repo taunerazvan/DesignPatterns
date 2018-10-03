c3946f6121b008ea2367362ffa66dfca536b7658
razvan.taune <razvan.taune@rcs-rds.ro>
2018-10-03 18:01:22 +0300

    builder

---
+++ module/DesignPatterns/Structural/DependencyInjection/Test.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace DesignPatterns\Structural\DependencyInjection\Tests;
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once '/var/www/html/github/vendor/autoload.php';
+
+
+use DesignPatterns\Structural\DependencyInjection\DatabaseConfiguration;
+use DesignPatterns\Structural\DependencyInjection\DatabaseConnection;
+use PHPUnit\Framework\TestCase;
+
+class DependencyInjectionTest extends TestCase
+{
+    public function testDependencyInjection()
+    {
+        $config = new DatabaseConfiguration('localhost', 3306, 'domnikl', '1234');
+        $connection = new DatabaseConnection($config);
+
+        echo '<pre>';
+        print_r($connection);
+
+        $this->assertEquals('domnikl:1234@localhost:3306', $connection->getDsn());
+    }
+}
+
